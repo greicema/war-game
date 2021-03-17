f2bbf836e71e0d1e6ad9922922a2fb981cb5d6e6
Greice Mara <sgreicemaraj@gmail.com>
2021-03-17 17:30:48 -0300

    início e animação

---
+++ js/js.js
@@ -0,0 +1,21 @@
+function start() {
+    $("#inicio").hide();
+
+    $("#fundogame").append("<div id='jogador' class='anima1'></div>");
+    $("#fundogame").append("<div id='inimigo1' class='anima2'></div>");
+    $("#fundogame").append("<div id='inimigo2'></div>");
+    $("#fundogame").append("<div id='amigo' class='anima3'></div>");
+
+    var jogo = {};
+    //Game loop
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        movefundo();
+    }
+
+    function movefundo() {
+        esquerda = parseInt($("#fundogame").css("background-position"));
+        $("#fundogame").css("background-position", esquerda - 1);
+    }
+}
